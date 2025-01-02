25dcd119ea492c50a98c6f3abcbbe6ff2a3f1695
TpG <ltphu.dev@gmail.com>
2025-01-02 17:45:56 +0700

    Clear project to re-new it

--- lib/GUI/AnimationRenderer.cpp
+++
@@ -1,8 +0,0 @@
-#include "AnimationRenderer.hpp"
-
-void AnimationRenderer::next(const Animation &animation, Rectangle dest, Vector2 origin /*= Vector2{0}*/, float rotation /*= 0.0f*/, Color tint /*= WHITE*/)
-{
-    int index = (int)(GetTime() * animation.framePerSecond) % (int)animation.rectangles.size();
-    Rectangle source = animation.rectangles[index];
-    DrawTexturePro(animation.atlas, source, dest, origin, rotation, tint);
-}
